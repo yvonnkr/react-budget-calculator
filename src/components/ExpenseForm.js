29b7c83cc16e8ba110b6917bb2a0d7cec4a17748
Yvonne Nkr <yvonnkr86@gmail.com>
2019-10-02 16:35:06 +0100

    ExpenseForm setup

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { MdSend } from 'react-icons/md';
 
 const ExpenseForm = () => {
   return (
-    <div>
-      <h2>Hello form ExpenseForm</h2>
-    </div>
+    <form>
+      <div className='form-center'>
+        <div className='form-group'>
+          <label htmlFor='charge'>charge</label>
+          <input
+            type='text'
+            className='form-control'
+            id='charge'
+            name='charge'
+            placeholder='e.g. rent'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='amount'>amount</label>
+          <input
+            type='text'
+            className='form-control'
+            id='amount'
+            name='amount'
+            placeholder='e.g. 1000'
+          />
+        </div>
+      </div>
+      <button type='submit' className='btn'>
+        submit <MdSend className='btn-icon' />
+      </button>
+    </form>
   );
 };
 
